6c64675074f9bf59fc3d46c7fafd95c430242b80
Jaime Rodrigues Jr <jaimerodriguesjunior@outlook.com>
2026-02-28 18:04:42 -0300

    agendamento e impressao

--- notebooklm-agendamento/migration_add_alerta_adiado_ate.sql
+++
@@ -1,2 +0,0 @@
--- Criação do campo alerta_adiado_ate na tabela appointments
-ALTER TABLE appointments ADD COLUMN IF NOT EXISTS alerta_adiado_ate TIMESTAMPTZ;
--- notebooklm-agendamento/migration_add_confirmado_status.sql
+++
@@ -1,4 +0,0 @@
--- Adicionar 'confirmado' ao CHECK constraint da coluna status em appointments
-ALTER TABLE appointments DROP CONSTRAINT IF EXISTS appointments_status_check;
-ALTER TABLE appointments ADD CONSTRAINT appointments_status_check 
-  CHECK (status IN ('agendado', 'confirmado', 'em_atendimento', 'concluido', 'cancelado', 'nao_compareceu'));
